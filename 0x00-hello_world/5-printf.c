95f08c6dd02b2470b8eeb817dd03c181d7f091ca
root <alaa.badawy404@gmail.com>
2023-03-17 18:01:51 -0700

    edit 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
+
 /**
-*main-the main
-*description c program to write a fun
-*return always equals 0 , means successfully
+ * main - the main
+ *
+ * Description: c program that print with put func
+ *
+* Return: always equals 0 , means successfully.
 */
-int main (void)
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
