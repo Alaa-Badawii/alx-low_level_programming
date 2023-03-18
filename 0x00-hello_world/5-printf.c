b89e1c6ada20e95faa1453bbdd39c3970ec9f142
root <alaa.badawy404@gmail.com>
2023-03-17 18:00:21 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main-the main
+*description c program to write a fun
+*return always equals 0 , means successfully
+*/
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
