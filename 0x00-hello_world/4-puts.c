541fdec5fb200500f91e181d66774b33db1ff2b3
root <alaa.badawy404@gmail.com>
2023-03-17 17:52:32 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stio.h>
+
+/**
+*main-the main
+*description: c program that printes put fun
+*returns always equals 0 , mains successfully
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
