23a735d98972cb25ecc508918ec32e1592231b7a
root <alaa.badawy404@gmail.com>
2023-03-28 15:19:41 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is success
+ */
+
+int _strlen.c(char *c)
+{
+	int len=0;
+	
+	for (;*s++;)
+		len++;
+	
+	return (len);
+}
