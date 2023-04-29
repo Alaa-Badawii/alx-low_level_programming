d6724170704ef04f40bec4b12b597d781caa914f
Alaa Badawy <alaa.badawy404@gmail.com>
2023-04-29 10:06:05 -0700

    Deferring to AFR-0423 (C#15) starting on Wed 06/07

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,4 +0,0 @@
-#ifndef _HEADER_
-#define _HEADER_
-#define ABS(x) ((x) > 0 ? (x) : (-(x))) /* Using Tenary Statment */
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,4 +0,0 @@
-#ifndef _HEADER_
-#define _HEADER_
-#define SUM(x, y) ((y) + (x)) /* Using Tenary Statment */
-#endif
