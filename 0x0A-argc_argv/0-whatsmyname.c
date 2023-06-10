4c891e93dd9652fa075c584df85c0b31c0219bf1
Alaa-Badawii <alaa.badawy404@gmail.com>
2023-06-09 17:05:15 -0700

    h

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: 0 if successful
  */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc, char const *argv[])
 {
-	printf("%s\n", *argv);
-
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);	
 }	
