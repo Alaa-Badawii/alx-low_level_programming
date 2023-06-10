eaa1d3f55ead296bacf7794e601d186407f3f7ab
Alaa-Badawii <alaa.badawy404@gmail.com>
2023-06-09 18:04:40 -0700

    h

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-prints the minimum number of coins
+ * to make a change for an amount of money
+ * @agrc: number of arguments
+ * @argv: array of arguments
+ * return: 0-success, non-zero-fail.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+
+	printf("%d\n", sum);
+	
+	return(0);
+}
+
