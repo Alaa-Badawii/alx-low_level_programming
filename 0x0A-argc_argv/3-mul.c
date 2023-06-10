32193663021d8c7641e561309402edaf6eb3ba65
Alaa-Badawii <alaa.badawy404@gmail.com>
2023-06-09 17:56:36 -0700

    h

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main-prints the minimum number of coins
+ * main-prints sum of 2 numbers.
  * to make a change for an amount of money
- * @agrc: number of arguments
- * @argv: array of arguments
+ * @agrc: number of arguments.
+ * @argv: array of arguments.
  * return: 0-success, non-zero-fail.
  */
 int main(int argc, char *argv[])
 {
 	int sum;
 
-	if (argc == 2)
+	if (argc ==3)
 	{
 		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
