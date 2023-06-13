1e062a1aaa9ed81f760022074e8f74f74ca52c00
Alaa-Badawii <alaa.badawy404@gmail.com>
2023-06-12 18:39:09 -0700

    edit betty style

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 /**
  * main-prints sum of 2 numbers.
- * to make a change for an amount of money
- * @agrc: number of arguments.
- * @argv: array of arguments.
- * return: 0-success, non-zero-fail.
+ * Descripton: make a change for an amount of money
+ * @argc: the number of arguments.
+ * @argv: the array of arguments.
+ * Return: 0-success, non-zero-fail.
  */
 int main(int argc, char *argv[])
 {
 	int sum;
 
-	if (argc ==3)
+	if (argc == 3)
 	{
 		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
@@ -19,9 +19,7 @@ int main(int argc, char *argv[])
 	else
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
-
 	return (0);
-}	
-		
+}
