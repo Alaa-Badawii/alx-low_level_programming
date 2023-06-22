3d09fefcc17fa73ceb4ffc8fdbe3b2c5f9c4b84c
Alaa-Badawii <alaa.badawy404@gmail.com>
2023-06-22 16:50:56 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,21 +4,20 @@
 
 /**
  * sum_them_all-returns the sum of all its parameters.
- * @n:first fixed argument
- * Return: sum of all its parameters.
+ * @n:number of parameters passed to function
+ * Return: If n == 0 - 0.
+ * otherwise - sum of all its parameters.
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
+	unsigned int i, sum = 0;
 	va_list ptr;
 
 	if (n == 0)
 		return (0);
 
 	va_start(ptr, n);
-	int i;
-
 	for (i = 0; i < n; i++)
 	{
 		sum + = va_arg(ptr, int);
