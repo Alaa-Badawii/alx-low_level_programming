64de514f859b4621b4a0f7644dea58417b2c4678
Alaa-Badawii <alaa.badawy404@gmail.com>
2023-07-03 15:58:43 -0700

    edit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - returns the length of the string
+ * @n: string whose length to cheked
+ *
+ * Return: interger length of string
+ */
+int _strlen(char *n)
+{
+	int i = 0;
+
+	if (!n)
+		return (0);
+	while (*n++)
+		i++;
+	return (i);
+}
+
+/**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  *
@@ -9,7 +26,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t i  = 0;
 
 	while (h)
 	{
@@ -18,8 +35,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		i++;
 	}
 
-	return (s);
+	return (i);
 }
